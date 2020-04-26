558f4cca329dc0442d7af5e04314ad484159eb2b
Aibek <aibek9898@gmail.com>
2020-04-26 20:55:49 +0600

    Add files via upload

---
+++ src/main/java/org/example/MainServlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/****************************
+ * @author a.yertuyak
+ * @since 26.04.2020
+ ***************************/
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("Hello World!");
+    }
+}
