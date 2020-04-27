beaecbe35474046a4b2e949bde295fbcfd468663
Aibek <aibek9898@gmail.com>
2020-04-27 20:58:35 +0600

    Update MainServlet.java

--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -11,8 +11,17 @@ import java.io.PrintWriter;
  * @author a.yertuyak
  * @since 26.04.2020
  ***************************/
+@WebServlet("/users")
 public class MainServlet extends HttpServlet {
 
+    UserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        userRepository = new UserRepositoryImpl();
+    }
+
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
