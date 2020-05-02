91f5268bfbb21b7d6cfd88f6e0ceb1b2ee195a18
Aibek <aibek9898@gmail.com>
2020-05-02 12:05:10 +0600

    Update MainServlet.java

--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import org.example.models.User;
+import org.example.repos.UserRepository;
+import org.example.repos.UserRepositoryImpl;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 /****************************
  * @author a.yertuyak
@@ -24,7 +31,9 @@ public class MainServlet extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.println("Hello World!");
+        List<User> users = userRepository.findAll();
+        req.setAttribute("user", users);
+        RequestDispatcher dispatcher = req.getServletContext().getRequestDispatcher("/users.jsp");
+        dispatcher.forward(req, resp);
     }
 }
