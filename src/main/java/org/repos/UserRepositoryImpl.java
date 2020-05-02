cc7fc37856ceb20ba446cf7148c3c08956ad40d4
Aibek <aibek9898@gmail.com>
2020-05-02 12:05:40 +0600

    Update UserRepositoryImpl.java

--- src/main/java/org/repos/UserRepositoryImpl.java
+++ src/main/java/org/repos/UserRepositoryImpl.java
@@ -16,21 +16,21 @@ public class UserRepositoryImpl implements UserRepository {
 
     public UserRepositoryImpl(){
         users = new ArrayList<>();
-        try {
+          try {
+            Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", ""); //quiz - наша база
-            Statement stmt = con.createStatement();
+//            Statement stmt = con.createStatement();
             PreparedStatement pstm = con.prepareStatement("select * from users");
             ResultSet rs = pstm.executeQuery();
 
             if(rs.next()){
                 User user = new User();
-                user.setNickname(rs.getString(1));
-                user.setPassword(rs.getString(2));
+                user.setNickname(rs.getString(2));
+                user.setPassword(rs.getString(3));
                 users.add(user);
             }
 
-
-        } catch (SQLException throwables) {
+        } catch (SQLException | ClassNotFoundException throwables) {
             throwables.printStackTrace();
         }
 
